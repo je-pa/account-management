20fc25fd23a81228d2fd08bb95fe69b5045a2baa
je-pa <jieun0502@gmail.com>
2022-03-11 09:51:08 +0900

    2022.03.11_검색

--- src/main/resources/static/js/management.js
+++ src/main/resources/static/js/management.js
@@ -1,22 +1,15 @@
 function moveToPage(page){
     const url = new URL(window.location.href);
-    if(url.searchParams.has('page')){
-        url.searchParams.set('page',page);
-    }else{
-        url.searchParams.append('page',page);
-    }
+    url.searchParams.set('page',page);
     window.location.href=url.href;
 }
 
 function moveToUserSortIdPage(userSortId){
     let url = new URL(window.location.href);
     if(userSortId==='all'){
-        window.location.href='management';
-        return;
-    }else if(url.searchParams.has('userSortId')){
-        url.searchParams.set('userSortId',userSortId);
+        url.searchParams.delete('userSortId');
     }else{
-        url.searchParams.append('userSortId',userSortId);
+        url.searchParams.set('userSortId',userSortId);
     }
     url.searchParams.set('page','1');
     window.location.href=url.href;
@@ -27,24 +20,72 @@ function checkUserSortRadio(){
     const url = new URL(window.location.href);
     if(!url.searchParams.has('userSortId')){
         userSortRadio[0].checked=true;
-    }else if(url.searchParams.get('userSortId')=='2'){
-        userSortRadio[1].checked=true;
-    }else if(url.searchParams.get('userSortId')=='3'){
-        userSortRadio[2].checked=true;
-    }else if(url.searchParams.get('userSortId')=='4'){
-        userSortRadio[3].checked=true;
     }
+    userSortRadio.forEach(e=>{
+        if(e.value==url.searchParams.get('userSortId')){
+            e.checked=true;
+        }
+    })
 }
 checkUserSortRadio();
+
+const companySelectOption = document.querySelectorAll('.company_select option');
+
+function selectedCompanySelect(){
+    const url = new URL(window.location.href);
+    companySelectOption.forEach(e=>{
+        if(e.value==url.searchParams.get('company')){
+            e.selected=true;
+        }
+    })
+}
+selectedCompanySelect();
 
 function changeSelect(element){
     console.log(element.value);
     const url = new URL(window.location.href);
-    if(url.searchParams.has('company')){
-        url.searchParams.set('company',element.value);
+    // if(url.searchParams.has('company')){
+    //     url.searchParams.set('company',element.value);
+    // }else{
+    //     url.searchParams.append('company',element.value);
+    // }
+    if(element.value==='all'){
+        url.searchParams.delete('company');
     }else{
-        url.searchParams.append('company',element.value);
+        url.searchParams.set('company',element.value);
     }
     url.searchParams.set('page','1');
     window.location.href=url.href;
 }
+
+function search(element){
+    const url = new URL(window.location.href);
+    const searchWord = element.searchWord.value;
+    if(searchWord===''){
+        return false;
+    }
+    url.searchParams.set('search',searchWord);
+    window.location.href=url.href;
+    return false;
+}
+
+function searchWordInput(){
+    const url = new URL(window.location.href);
+    const searchWordElem = document.getElementsByName('searchWord');
+    searchWordElem[0].value=url.searchParams.get('search');
+}
+searchWordInput();
+
+function findSearchWord(){
+    const url = new URL(window.location.href);
+    if(url.searchParams.has('search')){
+        let search = url.searchParams.get('search');
+        $("td:contains('"+search+"')").css({color:"blue"});
+        //
+        // $("input:contains('"+search+"')").each(function () {
+        //     var regex = new RegExp(search,'gi');
+        //     $(this).html( $(this).text().replace(regex, "<span class='txt-hlight'>"+search+"</span>") );
+        // });
+    }
+}
+findSearchWord();
